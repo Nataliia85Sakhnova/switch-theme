7ea86e036e4b0d833eaf9e58ad009468a05c9e10
Razhatawka <nataliia85sakhnova@gmail.com>
2022-03-29 17:22:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+switch (color) {
+  case 'black':
+      return (black)
+      break;
+
+  case 'pink':
+    return (pink)
+    break;
+
+  case 'white':
+    return (white)
+    break;
+
+  case "blue":
+    return (blue)
+    break;
+}
